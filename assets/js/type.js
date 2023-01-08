92092a83162b06829f97472ff36e000fcc821c20
Dokza Equaristo <56497607+dokzaequaristo@users.noreply.github.com>
2023-01-08 16:33:46 +0700

    Add First Commit
    First Commit

---
+++ assets/js/type.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.animate', {
+  strings: ['Taxation', 'Accounting', 'Admin', 'Data Entry', 'Web Programmer', 'Data Analytics', 'Front-End Develpoer'],
+  backSpeed: 40,
+  typeSpeed: 80,
+  loop: true
+});
+
