ba590492db4633d8967bd702f5fc8a1773d651f8
Dokza Equaristo <56497607+dokzaequaristo@users.noreply.github.com>
2023-10-02 13:14:04 +0700

    Update type.js

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.animate', {
-  strings: ['Taxation', 'Accounting', 'Admin', 'Data Entry', 'Web Programmer', 'Data Analytics', 'Front-End Develpoer'],
+  strings: ['Taxation', 'Accounting', 'Admin', 'Data Entry', 'Web Programmer', 'Data Analytics', 'Front-End Developer'],
   backSpeed: 40,
   typeSpeed: 80,
   loop: true
